9183cbaf1baa20c829cbb2d8cbf6bd1ad5060590
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-05-16 17:43:42 -0300

    Trabalhando com listas que são somente para leitura

---
+++ ListaLeitura/Curso.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaLeitura
+{
+    public class Curso
+    {
+		private IList<Aula> _aulas;
+		private string _nome;
+		private string _instrutor;
+
+        public Curso(string nome, string instrutor)
+        {
+			this._aulas = new List<Aula>();
+            this._nome = nome;
+            this._instrutor = instrutor;
+        }
+
+		/* IList =>
+		 * é implementado por uma classe genérica => nessecidade de passar também qual o tipo de elemento
+		 * implementado sempre que se usa o ReadOnlyCollection
+		 */
+
+        public IList<Aula> Aulas
+		{
+			get { return new ReadOnlyCollection<Aula>(_aulas); }
+            
+			/* remoção do set para proteger a lista de aulas:
+			 * set { _aulas = value; }
+			 */
+           
+        }
+
+        public string Nome
+		{
+			get { return _nome; }
+			set { _nome = value; }
+		}
+
+		public string Instrutor
+		{
+			get { return _instrutor; }
+			set { _instrutor = value; }
+		}
+
+        internal void Adiciona(Aula aula)
+        {
+            this._aulas.Add(aula);
+        }
+    }
+}
---
+++ ListaLeitura/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaLeitura
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Curso cSharpColecoes = new Curso("C# Collections", "Marcelo Oliveira");
+
+            /*code smell => chamado de exposição indecente (neste caso)
+             * a classe Curso não tem nenhum controle sobre as suas aulas, sobre sua lista de aulas; a lista esta exposta;
+             * o correto seria desenvolver um método na classe Curso de modo que a adição de um novo curso fosse feita por lá;
+             * necessidade de encapsular a lista
+             */
+            /*cSharpColecoes.Aulas.Add(new Aula("Trabalhando com Listas", 21));
+             * devido à proteção (encapsulamento) da lista na classe Curso, não é possível adicionar um elemento (impossibilidade de modificação)
+             */
+
+            cSharpColecoes.Adiciona(new Aula("Trabalhando com listas", 21));
+            Imprimir(cSharpColecoes.Aulas);
+
+            Console.ReadKey();
+        }
+
+        private static void Imprimir(IList<Aula> aulas)
+        {
+            Console.Clear();
+            foreach (var aula in aulas)
+            {
+                Console.WriteLine(aula);
+            }
+        }
+    }
+}
