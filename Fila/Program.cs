b36f7006572e609b5f8d1e784a543164c7d9fbcb
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-05-18 17:22:25 -0300

    Aprendendo a trabalhar com filas

---
+++ Fila/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fila
+{
+    public class Program
+    {
+        //Queue => fila => o primeiro elemento a entrar é o primeiro a sair
+        static Queue<string> pedagio = new Queue<string>();
+
+        static void Main(string[] args)
+        {
+            //adicionando elementos à fila => método Enqueue (enfileirar)
+            Enfileirar("van");
+            Enfileirar("kombi");
+            Enfileirar("guincho");
+            Enfileirar("pickup");
+
+            //liberando carros => método Dequeue (desenfileirar) => retorna o elemento que saiu
+            Desenfileirar();
+            Desenfileirar();
+            Desenfileirar();
+            Desenfileirar();
+
+        }
+
+        private static void Desenfileirar()
+        {
+            if (pedagio.Any())
+            {
+                if(pedagio.Peek() == "guincho")
+                {
+                    Console.WriteLine("Guincho está fazendo o pagamento");
+                }
+                string veiculo = pedagio.Dequeue();
+                Console.WriteLine($"Saiu da fila: {veiculo}");
+                ImprimirFila();
+            }
+        }
+
+        private static void Enfileirar(string veiculo)
+        {
+            Console.WriteLine($"Entrou na fila: {veiculo}");
+            pedagio.Enqueue(veiculo);
+            ImprimirFila();
+        }
+
+        private static void ImprimirFila()
+        {
+            //exibindo elementos da fila
+            foreach (var car in pedagio)
+            {
+                Console.WriteLine($"Fila: {car}");
+            }
+        }
+    }
+}
