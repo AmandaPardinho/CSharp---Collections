2659acfbf3571b28a081d541d23e5b1f0e3bbf60
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-05-18 14:21:45 -0300

    Aprendendo a manipular pilhas

---
+++ NavegacaoBrowserStack/Navegador.cs
@@ -0,0 +1,40 @@
+﻿namespace NavegacaoBrowserStack
+{
+    public class Navegador
+    {
+        //Stack = pilha  
+        private readonly Stack<string> historicoAnterior = new Stack<string>();
+        private readonly Stack<string> historicoProximo = new Stack<string>();
+        private string atual = "vazia";
+
+        public Navegador()
+        {
+            Console.WriteLine($"Página atual: {atual}");
+        }
+
+        public void Anterior()
+        {
+            if (historicoAnterior.Any())
+            {
+                historicoProximo.Push(atual);
+                //método Pop => pega o próximo elemento de uma pilha e retorna uma string
+                atual = historicoAnterior.Pop();
+                Console.WriteLine($"Página atual: {atual}");
+            }
+        }
+
+        public void NavegarPara(string url)
+        {
+            //método Push => adiciona um elemento a uma pilha
+            historicoAnterior.Push(atual);
+            atual = url;
+            Console.WriteLine($"Página atual: {atual}");
+        }
+
+        public void Proximo()
+        {
+            atual = historicoProximo.Pop();
+            Console.WriteLine($"Página atual: {atual}");
+        }
+    }
+}
---
+++ NavegacaoBrowserStack/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NavegacaoBrowserStack
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //definindo novo objeto
+            var navegador = new Navegador();
+
+            //criando um método para mostrar a alteração entre url
+            navegador.NavegarPara("google.com");
+            navegador.NavegarPara("caelum.com.br");
+            navegador.NavegarPara("alura.com.br");
+
+            navegador.Anterior();
+            navegador.Anterior();
+            navegador.Anterior();
+            //não é possível usar o método Pop numa pilha vazia => necessário verificar
+            navegador.Anterior();
+
+            navegador.Proximo();
+        }
+    }
+}
